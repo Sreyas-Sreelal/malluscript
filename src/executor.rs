c0336c89638d6570d14bffb03c32a513c403e96e
SyS <sreyassreelal@gmail.com>
2020-04-01 20:37:42 +0530

    evaluation of logical expressions and conditional blocks

--- src/executor.rs
+++ src/executor.rs
@@ -34,8 +34,8 @@ impl Executor {
 
     pub fn execute(&mut self, unit: SourceUnit) {
         for x in unit.0 {
-            match x {
-                SourceUnitPart::Statement(stmt) => match stmt {
+            if let SourceUnitPart::Statement(stmt) = x { 
+                match stmt {
                     Statement::Declaration(symbol) => {
                         if let TokenType::Symbol(name) = symbol {
                             if !self
@@ -47,8 +47,18 @@ impl Executor {
                             }
                         }
                     },
+                    Statement::Conditional(expr,truebody,falsebody) => {
+                        let truth = (self.eval_arithmetic_logic_expression(expr).unwrap()) != 0;
+                        if truth {
+                            self.execute(truebody);
+                        } else {
+                            if let Some(body) = falsebody {
+                                self.execute(body);
+                            }
+                        }
+                    }
                     Statement::WriteExpr(expr) => {
-                        print!("{}", self.eval_arithmetic_expression(expr).unwrap());
+                        print!("{}", self.eval_arithmetic_logic_expression(expr).unwrap());
                     }
                     Statement::WriteString(token) => {
                         if let TokenType::Literal(string) = token {
@@ -62,7 +72,7 @@ impl Executor {
                                     panic!("Undefined symbol {}",identifier);
                                 }
                                 //if let literal = Expression::
-                                self.symbol_table.insert(identifier,DataTypes::Integer(self.eval_arithmetic_expression(*r).unwrap()));
+                                self.symbol_table.insert(identifier,DataTypes::Integer(self.eval_arithmetic_logic_expression(*r).unwrap()));
                                 //println!("{:?}",self.symbol_table);
                             } else {
                                 panic!("Invalid left assignment operator expected a symbol")
@@ -72,19 +82,23 @@ impl Executor {
                             //println!("somethinf elese {:?}", e);
                         }
                     },
-                
                     _ => {}
-                },
-                _ => {}
+                }
             }
         }
     }
-    fn eval_arithmetic_expression(&self,expr:Expression) -> Result<i64,&'static str> {
+
+    
+    fn eval_arithmetic_logic_expression(&self,expr:Expression) -> Result<i64,&'static str> {
         match expr {
-            Expression::Add(l,r) => Ok(self.eval_arithmetic_expression(*l).unwrap() + self.eval_arithmetic_expression(*r).unwrap()),
-            Expression::Multiply(l,r) => Ok(self.eval_arithmetic_expression(*l).unwrap() * self.eval_arithmetic_expression(*r).unwrap()),
-            Expression::Subtract(l,r) => Ok(self.eval_arithmetic_expression(*l).unwrap() - self.eval_arithmetic_expression(*r).unwrap()),
-            Expression::Divide(l,r) => Ok(self.eval_arithmetic_expression(*l).unwrap() / self.eval_arithmetic_expression(*r).unwrap()),
+            Expression::Add(l,r) => Ok(self.eval_arithmetic_logic_expression(*l).unwrap() + self.eval_arithmetic_logic_expression(*r).unwrap()),
+            Expression::Multiply(l,r) => Ok(self.eval_arithmetic_logic_expression(*l).unwrap() * self.eval_arithmetic_logic_expression(*r).unwrap()),
+            Expression::Subtract(l,r) => Ok(self.eval_arithmetic_logic_expression(*l).unwrap() - self.eval_arithmetic_logic_expression(*r).unwrap()),
+            Expression::Divide(l,r) => Ok(self.eval_arithmetic_logic_expression(*l).unwrap() / self.eval_arithmetic_logic_expression(*r).unwrap()),
+            Expression::Equals(l,r) => Ok((self.eval_arithmetic_logic_expression(*l).unwrap() == self.eval_arithmetic_logic_expression(*r).unwrap())as i64),
+            Expression::NotEquals(l,r) => Ok((self.eval_arithmetic_logic_expression(*l).unwrap() != self.eval_arithmetic_logic_expression(*r).unwrap())as i64),
+            Expression::GreaterThan(l,r) => Ok((self.eval_arithmetic_logic_expression(*l).unwrap() > self.eval_arithmetic_logic_expression(*r).unwrap())as i64),
+            Expression::LessThan(l,r) => Ok((self.eval_arithmetic_logic_expression(*l).unwrap() < self.eval_arithmetic_logic_expression(*r).unwrap())as i64),
             Expression::Integer(l) => {
                 match l {
                     TokenType::Number(number) => {
